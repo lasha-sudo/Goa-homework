dd20d9bee322b8fe872156adeaf3b3d78b6682ec
lasha-sudo <mekokishvili1011@gmail.com>
2025-01-27 19:33:58 +0400

    second commit

---
+++ level 31/home/ind.py
@@ -0,0 +1,39 @@
+#1
+name = 'lasha'
+place = 'Tbilisi'
+
+print(f"Hello my name is {name} and i live in {place}")
+
+#2
+list =', '.join(["apple", "banana", "cherry"])
+print(list)
+
+#3
+sentence = "The quick brown fox jumps over the lazy dog."
+filterd_text = sentence.split(" ")
+
+print(filterd_text)
+
+#4
+mixed_case = "PyThOn Is AwEsOmE!"
+
+lower_case = mixed_case.lower()
+
+print(lower_case)
+
+#5
+
+mixed_case2="good morning"
+
+upper_case = mixed_case2.upper()
+
+print(upper_case)
+
+
+
+
+
+
+
+
+
