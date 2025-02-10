5e6d39d4983a9aa7134d68b7a893496d4eae92b3
lasha-sudo <mekokishvili1011@gmail.com>
2025-02-10 10:41:49 +0400

    second commit

--- level 31/class/ind.py
+++ level 31/class/ind.py
@@ -1,4 +1,4 @@
 #1
 info = ["Lasha" , "Mekokishvili" , 14]
 
-print("My name is {0} my last name is {1} , my age is ")
+print("My name is {0} my last name is {1} , my age is  {2}".format(info[0] , info[1] , info[2]))
