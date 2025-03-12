717a601242c6b9492f09c19fd227855e02d7d33e
lasha-sudo <mekokishvili1011@gmail.com>
2025-03-12 16:19:06 +0400

    second commit

---
+++ level 42/class/ind.py
@@ -0,0 +1,13 @@
+
+def mouth_size(animal): 
+    animal2 = animal.lower()
+    if animal2 == 'alligator':
+        return 'small'
+    else:
+        return 'wide'
+
+
+
+#2
+def past(h, m, s):
+    return (3600h + 60m + s) * 1000
