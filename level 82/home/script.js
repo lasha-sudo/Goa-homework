21398276718c585e0e2b52c7ad39f02390330573
lasha-sudo <mekokishvili1011@gmail.com>
2025-08-01 15:01:41 +0400

    second commit

--- level 82/home/script.js
+++ level 82/home/script.js
@@ -28,11 +28,39 @@ allFalsy();
 
 
 //3
+function removeFalsy(arr) {
+    let result = [];
+    for (let i of arr) {
+        if (i) {
+            result.push(i)
+        }
+    }
+    return result
+}
+
+console.log(removeFalsy([0,1,'',null,true,false,'hello']))
 
 
 
 
 //4
+console.log('----')
+function countTruthiesAndFalsies(arr) {
+    let truthy = 0;
+    let falsy = 0;
+
+    for (let i of arr) {
+        if (i) {
+            truthy++;
+        } else {
+            falsy++;
+        }
+    }
+
+    return { truthy, falsy }
+
+}
+console.log(countTruthiesAndFalsies([0,1,'',null,true,false,'hello']))
 
 
 //5
@@ -74,9 +102,13 @@ let object = {
 
 let aray = [1,3,true,'kiwi'];
 
+console.log('use for in..');
+
 for(let i in object) {
     console.log(i)
 };
+
+console.log('use for of..');
 
 for(let i of aray) {
     console.log(i)
