f197c87445f488c202d03008e5a9bdfc526dac02
lasha-sudo <mekokishvili1011@gmail.com>
2026-01-20 06:42:16 -0800

    second commit

---
+++ level 128/home/vite-project/src/Nav.jsx
@@ -0,0 +1,18 @@
+function Nav(){
+    return(
+        <nav className="flex justify-between px-6 py-4 items-center ">
+            <h3 className="font-bold text-3xl" >digital bank</h3>
+
+            <ul className="flex flex-row gap-3 h-auto">
+                <li className="text-sm  hover:text-blue-700">Home</li>
+                <li className="text-sm  hover:text-blue-700">about</li>
+                <li className="text-sm  hover:text-blue-700">contact</li>
+                <li className="text-sm  hover:text-blue-700">block</li>
+            </ul>
+
+            <button className="bg-blue-600 text-white rounded-[5px] hover:bg-blue-900 w-29 h-8">requist invite</button>
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ level 128/home/vite-project/src/Section.jsx
@@ -0,0 +1,14 @@
+
+function Section() {
+
+
+    return(
+        <div>
+            <h1>Next generational 
+                banking
+            </h1>
+
+            <p></p>
+        </div>
+    )
+}
